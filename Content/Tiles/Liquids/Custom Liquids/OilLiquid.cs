9c3654bb77183fd426188a0dcdf355385fc718bf
IsmailAlsaad <159543636+IsmailAlsaad@users.noreply.github.com>
2026-09-07 03:15:27 +0300

    Create OilLiquid.cs

---
+++ Content/Tiles/Liquids/Custom Liquids/OilLiquid.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorraria.Content.Tiles.Liquids.Custom_Liquids
+{
+    internal class OilLiquid
+    {
+    }
+}
