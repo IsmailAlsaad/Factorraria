403bc9744f7b9e3d81d854eac0cca27c1e6545e2
IsmailAlsaad <159543636+IsmailAlsaad@users.noreply.github.com>
2026-09-13 19:20:01 +0300

    Update IceBladeSwingProjectile.cs

--- Content/Projectiles/Weapons/IceBladeSwingProjectile.cs
+++ Content/Projectiles/Weapons/IceBladeSwingProjectile.cs
@@ -1,22 +1,21 @@
 ﻿using Factorraria.Content.Configs;
+using Luminance.Common.Easings;
 using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
-using static tModPorter.ProgressUpdate;
 
 namespace Factorraria.Content.Projectiles.Weapons
 {
     public class IceBladeSwingProjectile : ModProjectile
     {
-        Vector2 SwordHoldOffset = new Vector2(-14, 16);
+        private float START_ANGLE = MathHelper.ToRadians(-30f); // Starting angle of the swing
+        private float END_ANGLE = MathHelper.ToRadians(60f); // Ending angle of the swing
+        private float SWING_SPEED = 0.3f; // Speed of the swing (in seconds)
+
         private ref float Timer => ref Projectile.ai[1]; // Timer to keep track of progression of each stage
         bool isFacingRight => Main.MouseWorld.X > Owner.MountedCenter.X;
 
@@ -43,8 +42,8 @@ namespace Factorraria.Content.Projectiles.Weapons
             Projectile.DamageType = DamageClass.Melee; // Projectile is a melee projectile
 
 
-            //DrawOriginOffsetX = (int)config.OffsetX;
-            //DrawOriginOffsetY = (int)config.OffsetY;
+            DrawOriginOffsetX = (int)config.OffsetX;
+            DrawOriginOffsetY = (int)config.OffsetY;
         }
 
         //public override void OnSpawn(IEntitySource source)
@@ -75,31 +74,46 @@ namespace Factorraria.Content.Projectiles.Weapons
 
         bool KillProjectileCondition() 
         {
-            return Owner.releaseUseItem || Timer >= TimerCap;
+            return Timer >= TimerCap;
         }
 
-        float TimerAngleOffset()
+        float releaseSwingAngle = 0f;
+        bool releaseSwingAngleSet = false;
+        float toMouseAngle = 0f;
+        float AngleOffsetByTimer()
         {
-            float angleOffset = (Timer / TimerCap) * MathHelper.PiOver2;
-            return isFacingRight ? -angleOffset : angleOffset;
+            float angleOffset = START_ANGLE + EasingCurves.Cubic.Evaluate(EasingType.Out,(Timer / TimerCap)) * END_ANGLE;
+
+            if(!releaseSwingAngleSet)
+            {
+                if (Owner.releaseUseItem)
+                {
+                    releaseSwingAngle = angleOffset;
+                    Timer = 0;
+                    TimerCap = SWING_SPEED * 60f;
+                    toMouseAngle = (Main.MouseWorld - Owner.MountedCenter).ToRotation();
+                    releaseSwingAngleSet = true;
+                }
+            }
+            else
+            {
+                angleOffset = releaseSwingAngle + EasingCurves.Cubic.Evaluate(EasingType.Out, (Timer / TimerCap)) * toMouseAngle;
+            }
+            
+            return isFacingRight ? -angleOffset : angleOffset - MathHelper.Pi;
         }
 
         public void SetSwordPosition()
-        {
+        {   
             Projectile.spriteDirection = isFacingRight ? 1 : -1;
             Owner.direction = Projectile.spriteDirection;
 
-            Vector2 toMouse = Main.MouseWorld - Owner.MountedCenter;
-            //Main.NewText("toMouse: " + toMouse.ToString());
-            toMouse = toMouse.RotatedBy(TimerAngleOffset());
-            //Main.NewText("toMouseRotated: " + toMouse.ToString());
+            float currentAngle = AngleOffsetByTimer();
 
-            float targetRotation = toMouse.ToRotation();
-            Projectile.rotation = targetRotation;
-            Projectile.rotation += Projectile.spriteDirection == 1 ? 0 : MathHelper.ToRadians(180f);
+            Projectile.rotation = currentAngle;
 
-            Owner.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, targetRotation - MathHelper.PiOver2);
-            Vector2 armPosition = Owner.GetFrontHandPosition(Player.CompositeArmStretchAmount.Full, targetRotation - MathHelper.PiOver2);
+            Owner.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, currentAngle - MathHelper.PiOver2);
+            Vector2 armPosition = Owner.GetFrontHandPosition(Player.CompositeArmStretchAmount.Full, currentAngle - MathHelper.PiOver2);
 
             if (Owner.gravDir == -1f)
             {
@@ -109,11 +123,38 @@ namespace Factorraria.Content.Projectiles.Weapons
 
             armPosition.Y += Owner.gfxOffY;
 
-            Vector2 NormalOffset = new Vector2(toMouse.Y, -toMouse.X).SafeNormalize(Vector2.Zero) * 16f;
-            Projectile.Center = armPosition + Vector2.Normalize(toMouse) * 16f + NormalOffset * Projectile.spriteDirection;
+            Projectile.Center = armPosition;
             Projectile.scale = Owner.GetAdjustedItemScale(Owner.HeldItem);
 
             Owner.heldProj = Projectile.whoAmI;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            // Calculate origin of sword (hilt) based on orientation and offset sword rotation (as sword is angled in its sprite)
+            Vector2 origin;
+            float rotationOffset;
+            SpriteEffects effects;
+
+            if (Projectile.spriteDirection > 0)
+            {
+                origin = new Vector2(0, Projectile.height);
+                rotationOffset = 0f;
+                effects = SpriteEffects.None;
+            }
+            else
+            {
+                origin = new Vector2(Projectile.width, Projectile.height);
+                rotationOffset = MathHelper.PiOver2;
+                effects = SpriteEffects.FlipHorizontally;
+            }
+
+            Texture2D texture = TextureAssets.Projectile[Type].Value;
+
+            Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, default, lightColor * Projectile.Opacity, Projectile.rotation + rotationOffset, origin, Projectile.scale, effects, 0);
+
+            // Since we are doing a custom draw, prevent it from normally drawing
+            return false;
         }
 
         //public void SetSwordPosition()
